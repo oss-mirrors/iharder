84ff3a106d0c3bd56b2afb6a48cbfc0c288020b3
rharder <rharder@71732433-81d3-41b0-bfc5-5a78a3658b4e>
2008-10-23 05:40:22 +0000

    Moved main() out of FileDrop for security reasons and added Example.java to hold the main() method.
    git-svn-id: svn://svn.code.sf.net/p/iharder/code@49 71732433-81d3-41b0-bfc5-5a78a3658b4e

--- filedrop/src/FileDrop.java
+++ filedrop/src/FileDrop.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.Reader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * This class makes it easy to drag and drop files from the operating
@@ -598,38 +596,6 @@ public class FileDrop
     }   // end remove
     
     
-    
-    
-    
-
-    /** Runs a sample program that shows dropped files */
-    public static void main( String[] args )
-    {
-        javax.swing.JFrame frame = new javax.swing.JFrame( "FileDrop" );
-        //javax.swing.border.TitledBorder dragBorder = new javax.swing.border.TitledBorder( "Drop 'em" );
-        final javax.swing.JTextArea text = new javax.swing.JTextArea();
-        frame.getContentPane().add( 
-            new javax.swing.JScrollPane( text ), 
-            java.awt.BorderLayout.CENTER );
-        
-        new FileDrop( System.out, text, /*dragBorder,*/ new FileDrop.Listener()
-        {   public void filesDropped( java.io.File[] files )
-            {   for( int i = 0; i < files.length; i++ )
-                {   try
-                    {   text.append( files[i].getCanonicalPath() + "\n" );
-                    }   // end try
-                    catch( java.io.IOException e ) {}
-                }   // end for: through each dropped file
-            }   // end filesDropped
-        }); // end FileDrop.Listener
-
-        frame.setBounds( 100, 100, 300, 400 );
-        frame.setDefaultCloseOperation( frame.EXIT_ON_CLOSE );
-        frame.show();
-    }   // end main
-
-
-
 
     
 /* ********  I N N E R   I N T E R F A C E   L I S T E N E R  ******** */    
