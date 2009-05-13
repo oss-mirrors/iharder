a286bdc96d4eb8ce551bac0acf0b1dd47cc618bc
rharder <rharder@71732433-81d3-41b0-bfc5-5a78a3658b4e>
2009-05-13 20:59:50 +0000

    refine video launch
    git-svn-id: svn://svn.code.sf.net/p/iharder/code@106 71732433-81d3-41b0-bfc5-5a78a3658b4e

--- rvision/cameracontrol/Launch Servers.bat
+++ rvision/cameracontrol/Launch Servers.bat
@@ -1,14 +1,15 @@
 cd dist
-start java -cp RVision.jar rvision.UdpCameraServer -s COM1 -p 4000
-start java -cp RVision.jar rvision.HttpCameraServer 4000 4002
+rem start java -cp RVision.jar rvision.UdpCameraServer -s COM1 -p 4000
+rem start java -cp RVision.jar rvision.HttpCameraServer 4000 4002
 
 SET GROUP=225.0.0.1
 SET RTP_PORT=6970
 SET HTTP_PORT=6972
 SET SDP_MULTI_FILE=c:\wamp\www\camera\multicast.sdp
 SET VLC=C:\Program Files\VideoLAN\VLC\VLC.EXE
+SET BITRATE=1024
 REM SET VDEV=ICatch (VI) PC Camera
-SET VDEV=USB 2820 Device
+SET VDEV=USB 2820 Video
 
 rem SET SIZE=640x480
 SET SIZE=320x240
@@ -16,7 +17,8 @@ SET SIZE=320x240
 REM
 REM This one shows a preview window
 REM
-REM "%VLC%" dshow:// :dshow-vdev="%VDEV%" :dshow-adev="none" :dshow-size="%SIZE%" --sout #transcode{vcodec=mp4v,vb=512,scale=1,fps=10}:duplicate{dst=display,dst=std{access=http,mux=ts,dst=:%HTTP_PORT%},dst=rtp{dst=127.0.0.1,port=6974,sdp=file://c:\Movies\camera.sdp}}
+REM
+ "%VLC%" dshow:// :dshow-vdev="%VDEV%" :dshow-adev="none" :dshow-size="%SIZE%" --sout #transcode{vcodec=mp4v,vb=%BITRATE%,scale=1,fps=10}:duplicate{dst=display,dst=std{access=http,mux=ts,dst=:%HTTP_PORT%},dst=rtp{dst=127.0.0.1,port=6974,sdp=file://c:\Movies\camera.sdp}}
 
 
 
@@ -24,7 +26,6 @@ REM "%VLC%" dshow:// :dshow-vdev="%VDEV%" :dshow-adev="none" :dshow-size="%SIZE%
 
 REM
 REM This one DOES NOT show a preview window
-REM
- "%VLC%" dshow:// :dshow-vdev="%VDEV%" :dshow-adev="none" :dshow-size="%SIZE%" --sout #transcode{vcodec=mp4v,vb=512,scale=1,fps=10}:duplicate{dst=std{access=http,mux=ts,dst=:%HTTP_PORT%},dst=rtp{dst=127.0.0.1,port=6974,sdp=file://c:\Movies\camera.sdp}}
+REM "%VLC%" dshow:// :dshow-vdev="%VDEV%" :dshow-adev="none" :dshow-size="%SIZE%" --sout #transcode{vcodec=mp4v,vb=%BITRATE%,scale=1,fps=10}:duplicate{dst=std{access=http,mux=ts,dst=:%HTTP_PORT%},dst=rtp{dst=127.0.0.1,port=6974,sdp=file://c:\Movies\camera.sdp}}
 
 
