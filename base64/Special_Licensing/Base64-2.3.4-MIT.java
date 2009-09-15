4cee878b4a7da8225a934d19808e43dac1843289
rharder <rharder@71732433-81d3-41b0-bfc5-5a78a3658b4e>
2009-09-15 19:28:17 +0000

    Special MIT release for v2.3.4.
    git-svn-id: svn://svn.code.sf.net/p/iharder/code@143 71732433-81d3-41b0-bfc5-5a78a3658b4e

--- base64/Special_Licensing/Base64-2.3.4-MIT.java
+++ base64/Special_Licensing/Base64-2.3.4-MIT.java
@@ -1,4 +1,29 @@
 /**
+ * <p>
+ * This file is licensed under the MIT License as read at
+ * http://www.opensource.org/licenses/mit-license.php
+ * and reproduced here:</p>
+ *
+ * <blockquote>Copyright (c) 2009 Robert Harder.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.</blockquote>
+ *
+ *
  * <p>Encodes and decodes to and from Base64 notation.</p>
  * <p>Homepage: <a href="http://iharder.net/base64">http://iharder.net/base64</a>.</p>
  * 
@@ -123,9 +148,6 @@
  * </ul>
  *
  * <p>
- * I am placing this code in the Public Domain. Do with it as you will.
- * This software comes with no guarantees or warranties but with
- * plenty of well-wishing instead!
  * Please visit <a href="http://iharder.net/base64">http://iharder.net/base64</a>
  * periodically to check for updates or to contribute improvements.
  * </p>
